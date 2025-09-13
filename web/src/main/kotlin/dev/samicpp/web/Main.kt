6faea370564584fbf803d926e4dfcb2bbe7f1bda
samic++ <samichentouf+git2@gmail.com>
2025-09-13 16:55:24 +0200

    setup simple tcp echo server

--- web/src/main/kotlin/dev/samicpp/web/Main.kt
+++ web/src/main/kotlin/dev/samicpp/web/Main.kt
@@ -1,8 +1,12 @@
 package dev.samicpp.web
 
 import dev.samicpp.http.example
+import dev.samicpp.http.TcpSocket
+import java.net.ServerSocket
 
-fun main() {
+val esc="\u001b"
+
+fun old_main() {
     println("main func called")
     example()
     var some=0
@@ -10,5 +14,24 @@ fun main() {
         println("loop $some")
         some+=1
         Thread.sleep(2000)
+    }
+}
+
+fun main(){
+    val server=ServerSocket(3456)
+    println("listening")
+    while (true){
+        val conn=server.accept()
+        println("accepted message from ${conn.remoteSocketAddress.toString()}")
+        Thread.startVirtualThread{
+            val sock=TcpSocket(conn)
+            var buff=ByteArray(1024)
+            while (true){
+                val read=sock.read(buff)
+                val slice=buff.sliceArray(0..<read)
+                val str=slice.toString(Charsets.UTF_8) //Charsets.UTF_8
+                println("client said: \u001b[36m$str\u001b[0m")
+            }
+        }
     }
 }

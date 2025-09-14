78e0d11af705dbea11a070807dbc82c5cba58a4c
samic++ <samichentouf+git2@gmail.com>
2025-09-14 02:04:21 +0200

    setup simple http test server

--- web/src/main/kotlin/dev/samicpp/web/Main.kt
+++ web/src/main/kotlin/dev/samicpp/web/Main.kt
@@ -2,6 +2,7 @@ package dev.samicpp.web
 
 import dev.samicpp.http.example
 import dev.samicpp.http.TcpSocket
+import dev.samicpp.http.Http1Socket
 import java.net.ServerSocket
 
 val esc="\u001b"
@@ -22,7 +23,7 @@ fun echo_test(){
     println("listening")
     while (true){
         val conn=server.accept()
-        println("accepted message from ${conn.remoteSocketAddress.toString()}")
+        println("accepted connection from ${conn.remoteSocketAddress.toString()}")
         Thread.startVirtualThread{
             val sock=TcpSocket(conn)
             var buff=ByteArray(1024)
@@ -40,6 +41,24 @@ fun echo_test(){
     }
 }
 
+fun http_dump_test(){
+    val server=ServerSocket(3000)
+    println("listening")
+    while (true){
+        val conn=server.accept()
+        println("accepted connection from ${conn.remoteSocketAddress.toString()}")
+        Thread.startVirtualThread{
+            val hand=Http1Socket(TcpSocket(conn))
+            hand.read_client()
+            hand.close("hello from ${hand.client.path}\n")
+            hand.client.apply { 
+                println("\u001b[32mclient { \n   path: $path, \n   method: $method, \n   version: $version, \n   headers: $headers, \n   body[${body.size}]: \"${body.decodeToString()}\", \n}\u001b[0m")
+            }   
+        }
+    }
+}
+
 fun main(){
-    echo_test()
+    // echo_test()
+    http_dump_test()
 }

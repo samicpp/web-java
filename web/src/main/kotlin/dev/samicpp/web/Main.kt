3851658ef4e315705aecd90a7af93a847760dd90
samic++ <samichentouf+git2@gmail.com>
2025-09-20 16:22:13 +0200

    add http2 test & seperate test from main class

--- web/src/main/kotlin/dev/samicpp/web/Main.kt
+++ web/src/main/kotlin/dev/samicpp/web/Main.kt
@@ -24,47 +24,7 @@ fun old_main() {
     }
 }
 
-fun echo_test(){
-    val server=ServerSocket(3000)
-    println("listening")
-    while (true){
-        val conn=server.accept()
-        println("accepted connection from ${conn.remoteSocketAddress.toString()}")
-        Thread.startVirtualThread{
-            val sock=TcpSocket(conn)
-            var buff=ByteArray(1024)
-            while (true){
-                val read=sock.read(buff)
-                val slice=buff.sliceArray(0..<read)
-                val str=slice.toString(Charsets.UTF_8) //Charsets.UTF_8
-                println("client said [$read]: \u001b[36m$str\u001b[0m")
-                if (read==-1){
-                    println("client likely disconnected")
-                    break
-                }
-            }
-        }
-    }
-}
-
-fun http_dump_test(){
-    val server=ServerSocket(3000)
-    println("listening")
-    while (true){
-        val conn=server.accept()
-        println("accepted connection from ${conn.remoteSocketAddress.toString()}")
-        Thread.startVirtualThread{
-            val hand=Http1Socket(TcpSocket(conn))
-            hand.readClient()
-            hand.close("hello from ${hand.client.path}\n")
-            hand.client.apply { 
-                println("\u001b[32mclient { \n   path: $path, \n   method: $method, \n   version: $version, \n   headers: $headers, \n   body[${body.size}]: \"${body.decodeToString()}\", \n}\u001b[0m")
-            }   
-        }
-    }
-}
-
-fun server(){
+fun server(port:Int){
     val listener=ServerSocket(port,50,InetAddress.getByName(host))
     println("listening")
     while (true){
@@ -118,7 +78,8 @@ fun main(){
 
     setup()
 
-    HpackRoundTripTest.main()
+    // HpackRoundTripTest.main()
 
-    server()
+    server(port)
+    // http2frame_dump_test()
 }
---
+++ web/src/main/kotlin/dev/samicpp/web/test/Testing1.kt
@@ -0,0 +1,62 @@
+package dev.samicpp.web
+
+import dev.samicpp.http.example
+import dev.samicpp.http.TcpSocket
+import dev.samicpp.http.Http1Socket
+import dev.samicpp.http.Http2Connection
+import java.net.ServerSocket
+
+
+fun echo_test(){
+    val server=ServerSocket(3000)
+    println("listening")
+    while (true){
+        val conn=server.accept()
+        println("accepted connection from ${conn.remoteSocketAddress.toString()}")
+        Thread.startVirtualThread{
+            val sock=TcpSocket(conn)
+            var buff=ByteArray(1024)
+            while (true){
+                val read=sock.read(buff)
+                val slice=buff.sliceArray(0..<read)
+                val str=slice.toString(Charsets.UTF_8) //Charsets.UTF_8
+                println("client said [$read]: \u001b[36m$str\u001b[0m")
+                if (read==-1){
+                    println("client likely disconnected")
+                    break
+                }
+            }
+        }
+    }
+}
+
+fun http_dump_test(){
+    val server=ServerSocket(3000)
+    println("listening")
+    while (true){
+        val conn=server.accept()
+        println("accepted connection from ${conn.remoteSocketAddress.toString()}")
+        Thread.startVirtualThread{
+            val hand=Http1Socket(TcpSocket(conn))
+            hand.readClient()
+            hand.close("hello from ${hand.client.path}\n")
+            hand.client.apply { 
+                println("\u001b[32mclient { \n   path: $path, \n   method: $method, \n   version: $version, \n   headers: $headers, \n   body[${body.size}]: \"${body.decodeToString()}\", \n}\u001b[0m")
+            }   
+        }
+    }
+}
+
+fun http2frame_dump_test(){
+    val server=ServerSocket(3000)
+    println("listening")
+    while (true){
+        val conn=server.accept()
+        println("accepted connection from ${conn.remoteSocketAddress.toString()}")
+        Thread.startVirtualThread{
+            val hand=Http2Connection(TcpSocket(conn))
+            val frames=hand.incoming()
+            println(frames)
+        }
+    }
+}

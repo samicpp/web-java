f75190971a81903a8058023feb048d030ac9e398
samic++ <samichentouf+git2@gmail.com>
2025-10-07 08:52:30 +0200

    sync

--- web/src/main/java/dev/samicpp/web/Debug.java
+++ web/src/main/java/dev/samicpp/web/Debug.java
@@ -1,9 +1,10 @@
 package dev.samicpp.web;
 
 // in plain java only for the sake of testing interop
-
+import java.time.Instant;
 
 public class Debug {
+    Instant now=Instant.now();
     public static void start() {
         System.out.println("debug called");
     }
